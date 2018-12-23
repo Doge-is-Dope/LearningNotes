a6f33a452c78418af7291c43aae1ec984541fff2
Chun-Chieh Liang <maple@aples-MacBook-Pro.local>
2018-12-22 22:14:58 -0500

    constructor

--- Kotlin/KotlinProject/src/basics/Constructor.kt
+++ Kotlin/KotlinProject/src/basics/Constructor.kt
@@ -1,6 +1,20 @@
 package basics
 
+class Book(val title: String, val author: String, var price: Int) {
+    var message: String = "This is a message."
+
+    // secondary constructors
+    constructor(title: String, author: String, price: Int, message: String) : this(title, author, price) {
+        this.message = message
+    }
+}
 
 fun main(args: Array<String>) {
-    
+    val hp = Book("Harry Potter", "JK", 20)
+    println("Title: ${hp.title}, Author: ${hp.author}, Price: ${hp.price}, ${hp.message}")
+
+
+    val ho = Book("Harry Otter", "JK", 20, "Hi")
+    println("Title: ${ho.title}, Author: ${ho.author}, Price: ${ho.price}, ${ho.message}")
+
 }
