c00e3762442935062b43fa18f4a759f3c01faa65
Chun-Chieh Liang <maple3312936@outlook.com>
2019-01-30 16:29:46 -0500

    implicit intent

--- Android/NavigationArchitectureComponent/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Android/NavigationArchitectureComponent/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,23 +19,29 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.fragment_host)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
         // NavigationUI.setupWithNavController(binding.toolbar, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.fragment_host)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
