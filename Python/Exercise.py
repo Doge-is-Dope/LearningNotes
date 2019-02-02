dfa6bd7c6bec62180aa5b9f078c05948d0b3455d
Chun-Chieh Liang <maple3312936@outlook.com>
2019-02-01 22:24:05 -0500

    python data structure

--- Python/Exercise.py
+++ Python/Exercise.py
@@ -1,75 +1,45 @@
-"""Add a couple methods to our LinkedList class,
-and use that to implement a Stack.
-You have 4 functions below to fill in:
-insert_first, delete_first, push, and pop.
-Think about this while you're implementing:
-why is it easier to add an "insert_first"
-function than just use "append"?"""
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as
+few lines as possible.
+Make sure you pass the test cases too!"""
 
 
-class Element(object):
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-
-class LinkedList(object):
+class Queue:
     def __init__(self, head=None):
-        self.head = head
+        self.storage = [head]
 
-    def append(self, new_element):
-        current = self.head
-        if self.head:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
-            self.head = new_element
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
 
-    def insert_first(self, new_element):
-        "Insert new element as the head of the LinkedList"
-        new_element.next = self.head
-        self.head = new_element
+    def peek(self):
+        return self.storage[0]
 
-    def delete_first(self):
-        "Delete the first (head) element in the LinkedList as return it"
-        if self.head:
-            tmp = self.head
-            self.head = self.head.next
-            return tmp
-        else:
-            pass
+    def dequeue(self):
+        return self.storage.pop(0)
 
 
-class Stack(object):
-    def __init__(self, top=None):
-        self.ll = LinkedList(top)
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
 
-    def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        self.ll.insert_first(new_element)
+# Test peek
+# Should be 1
+print(q.peek())
 
-    def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
-        return self.ll.delete_first()
+# Test dequeue
+# Should be 1
+print(q.dequeue())
 
-
-# Test cases
-# Set up some Elements
-e1 = Element(1)
-e2 = Element(2)
-e3 = Element(3)
-e4 = Element(4)
-
-# Start setting up a Stack
-stack = Stack(e1)
-
-# Test stack functionality
-stack.push(e2)
-stack.push(e3)
-print(stack.pop().value)
-print(stack.pop().value)
-print(stack.pop().value)
-print(stack.pop())
-stack.push(e4)
-print(stack.pop().value)
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print(q.dequeue())
+# Should be 3
+print(q.dequeue())
+# Should be 4
+print(q.dequeue())
+q.enqueue(5)
+# Should be 5
+print(q.peek())
