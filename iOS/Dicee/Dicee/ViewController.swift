8572d4763c6e655292f94d4c39b087cd2ca75427
Chun-Chieh Liang <maple3312936@outlook.com>
2019-02-05 17:40:44 -0500

    iOS updated

---
+++ iOS/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Chun-Chieh Liang on 2/5/19.
+//  Copyright © 2019 Chun-Chieh Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
