fe2e2a8173b17f4831f4e15395f6a9ac2a2aecbf
Chun-Chieh Liang <maple3312936@outlook.com>
2019-02-05 22:30:47 -0500

    structured

--- Android/Codelab/shrine/README.md
+++
@@ -1,3 +0,0 @@
-# MDC-104 for Material Components for Android (Kotlin)
-
-Contains complete code structure for the MDC-104 Kotlin codelab.
--- iOS/Swift Basics.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var myAge: Int = 32
-myAge = 33
-
-let firstName = "Mike"
-let lastName = "Jordan"
-let fullName = "\(firstName) \(lastName)"
-print(fullName)
