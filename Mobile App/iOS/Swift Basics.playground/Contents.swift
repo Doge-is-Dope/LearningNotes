b18b5f744d11964c24ab1544b936c2c7ce65c869
Chun-Chieh Liang <maple3312936@outlook.com>
2019-02-11 01:16:24 -0500

    xylo

--- Mobile App/iOS/Swift Basics.playground/Contents.swift
+++ Mobile App/iOS/Swift Basics.playground/Contents.swift
@@ -7,8 +7,6 @@ let firstName = "Mike"
 let lastName = "Jordan"
 let fullName = "\(firstName) \(lastName)"
 
-
-
 // Calcaulate BMI
 func calculateBMI(weight: Double, height: Double) -> String {
     let bmi = weight / pow(height, 2)
@@ -26,26 +24,63 @@ func calculateBMI(weight: Double, height: Double) -> String {
     return "Your BMI is \(shotendBMI) and you are \(interpretation)"
 }
 
-// Calcaulate BMI imperial unit
-func calculateBMI2(weightInPounds: Double, heightInFoots: Double, heightRemainInches: Double) -> String {
-    
-    let bmi = weightInPounds * 0.45359237 / pow((heightInFoots * 12 + heightRemainInches) * 0.0254, 2)
-    let shotendBMI = String(format: "%1.2f", bmi)
-    var interpretation = ""
-    
-    
-    if bmi > 25 {
-        interpretation = "overweight"
-    } else if bmi > 18.5 {
-        interpretation = "normal weight"
-    } else {
-        interpretation =  "underweight"
-    }
-    return "Your BMI is \(shotendBMI) and you are \(interpretation)"
+print(calculateBMI(weight: 63, height: 1.8))
+
+let arrayOfNumbers = [1, 2, 3, 4, 5]
+var sum = 0
+for n in arrayOfNumbers  {
+    sum += n
 }
 
-print(calculateBMI(weight: 63, height: 1.8))
-print(calculateBMI2(weightInPounds: 140, heightInFoots: 5, heightRemainInches: 11))
+// steps in a for-in loop
+for n in 1...10  where n % 2 == 0 {
+//    print(n)
+}
+
+// steps in a for-loop
+for n in 1..<10  where n % 4 == 0 {
+//    print(n)
+}
 
 
+func beerSong(bottlesOfBeer totalNumber: Int)  {
+    var lyrics = ""
+    for num in (0...totalNumber).reversed() {
+        if num == 0 {
+            lyrics = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, \(totalNumber) bottles of beer on the wall."
+        } else {
+            lyrics = "\(num) bottles of beer on the wall, \(num) bottles of beer.\nTake one down and pass it around, \(num-1) bottles of beer on the wall."
+        }
+        print("\(lyrics)\n")
+    }
+}
+
+func fibonacci(until: Int) {
+    var previousBeforePrevious = 0
+    var previous = 1
+    print(previousBeforePrevious)
+    print(previous)
+    
+    // from iter 3
+    for _ in 0...until {
+        let current = previous + previousBeforePrevious
+        print(current)
+        previousBeforePrevious = previous
+        previous = current
+    }
+}
+
+
+func fibonacci2(until current: Int) -> Int {
+    if current == 1 {return 0}
+    else if current == 2 {return 1}
+    else {
+        return fibonacci2(until: current-2) + fibonacci2(until: current-1)
+    }
+}
+
+// 0, 1, 1, 2, 3, 5 , 8, 13, 21, 34, 55
+print(fibonacci2(until: 5))
+print("test", terminator: "")
+print("Gelo")
 
