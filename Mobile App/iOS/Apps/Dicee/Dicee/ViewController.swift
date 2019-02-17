886e9ae01a5af6c88913dd83251f8b5fd8280974
Chun-Chieh Liang <maple3312936@outlook.com>
2019-02-17 02:09:05 -0500

    dicee

--- Mobile App/iOS/Apps/Dicee/Dicee/ViewController.swift
+++ Mobile App/iOS/Apps/Dicee/Dicee/ViewController.swift
@@ -2,44 +2,54 @@
 //  ViewController.swift
 //  Dicee
 //
-//  Created by Chun-Chieh Liang on 2/5/19.
-//  Copyright © 2019 Chun-Chieh Liang. All rights reserved.
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
+
+var diceArray : Array = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+var firstRandomIndex : Int = 0
+var secondRandomIndex : Int = 0
+
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var firstDiceImageView: UIImageView!
     
-    let diceArray =  ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
-    
-    var randomDiceIndex1: Int = 0
-    var randomDiceIndex2: Int = 0
-    
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
-    
-    
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+    }
+
+    
+    
+    @IBAction func rollPressed(_ sender: AnyObject) {
         
         updateDiceImages()
     }
     
-    @IBAction func rollButtonPressed(_ sender: Any) {
-        updateDiceImages()
+    func updateDiceImages() {
+        
+        firstRandomIndex = Int(arc4random_uniform(6))
+        secondRandomIndex = Int(arc4random_uniform(6))
+        
+        firstDiceImageView.image = UIImage(named: diceArray[firstRandomIndex])
+        secondDiceImageView.image = UIImage(named: diceArray[secondRandomIndex])
     }
+    
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        updateDiceImages()
+        if motion == .motionShake {
+            updateDiceImages()
+        }
     }
     
-    func updateDiceImages(){
-        randomDiceIndex1 = Int.random(in: 0 ... 5)
-        randomDiceIndex2 = Int.random(in: 0 ... 5)
-        
-        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-    }
-    
+
+
 }
 
